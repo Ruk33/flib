b620dfb1297b8b072a5950cac4259100f2ba383b
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2024-01-13 14:33:01 -0300

    add docs and bin folder.

--- build.bat
+++ build.bat
@@ -1,7 +1,10 @@
 @echo off
 
 call vcvarsall x64 >NUL 2>NUL
-cl util.c -Drun_util && util.exe
-cl text.c -Drun_text && text.exe
-cl vecn.c -Drun_vecn && vecn.exe
-cl wqueue.c -Drun_wqueue && wqueue.exe
+
+pushd bin
+cl ../util.c -Drun_util && util.exe
+cl ../text.c -Drun_text && text.exe
+cl ../vecn.c -Drun_vecn && vecn.exe
+cl ../wqueue.c -Drun_wqueue && wqueue.exe
+popd bin
